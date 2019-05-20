49dd9768e8ebc4a13fbcbcc9acef92f69a8cad72
Zaytsev Aleksandr <47869933+SanyaNya@users.noreply.github.com>
2019-05-21 02:42:22 +0300

    Add files via upload

---
+++ MacrosCreator/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace MacrosCreator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.ListOfMacroses = new System.Windows.Forms.CheckedListBox();
+            this.CreateMacrosButton = new System.Windows.Forms.Button();
+            this.StartRecordingButton = new System.Windows.Forms.Button();
+            this.StopRecordingButton = new System.Windows.Forms.Button();
+            this.RepeatCheckBox = new System.Windows.Forms.CheckBox();
+            this.RepeatTimeTextBox = new System.Windows.Forms.TextBox();
+            this.RunMacrosesButton = new System.Windows.Forms.Button();
+            this.StopMacrosesButton = new System.Windows.Forms.Button();
+            this.MacrosNameLabel = new System.Windows.Forms.Label();
+            this.MacrosNameTextBox = new System.Windows.Forms.TextBox();
+            this.SetActivationKeyButton = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.DeleteSelected = new System.Windows.Forms.Button();
+            this.InfoButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ListOfMacroses
+            // 
+            this.ListOfMacroses.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ListOfMacroses.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.ListOfMacroses.FormattingEnabled = true;
+            this.ListOfMacroses.Location = new System.Drawing.Point(13, 13);
+            this.ListOfMacroses.Name = "ListOfMacroses";
+            this.ListOfMacroses.Size = new System.Drawing.Size(120, 229);
+            this.ListOfMacroses.TabIndex = 0;
+            // 
+            // CreateMacrosButton
+            // 
+            this.CreateMacrosButton.BackColor = System.Drawing.SystemColors.Info;
+            this.CreateMacrosButton.Location = new System.Drawing.Point(140, 167);
+            this.CreateMacrosButton.Name = "CreateMacrosButton";
+            this.CreateMacrosButton.Size = new System.Drawing.Size(150, 23);
+            this.CreateMacrosButton.TabIndex = 1;
+            this.CreateMacrosButton.Text = "Создать макрос";
+            this.CreateMacrosButton.UseVisualStyleBackColor = false;
+            this.CreateMacrosButton.Click += new System.EventHandler(this.CreateMacrosButton_Click);
+            // 
+            // StartRecordingButton
+            // 
+            this.StartRecordingButton.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.StartRecordingButton.Location = new System.Drawing.Point(140, 75);
+            this.StartRecordingButton.Name = "StartRecordingButton";
+            this.StartRecordingButton.Size = new System.Drawing.Size(75, 36);
+            this.StartRecordingButton.TabIndex = 2;
+            this.StartRecordingButton.Text = "Начать запись";
+            this.StartRecordingButton.UseVisualStyleBackColor = false;
+            this.StartRecordingButton.Click += new System.EventHandler(this.StartRecordingButton_Click);
+            // 
+            // StopRecordingButton
+            // 
+            this.StopRecordingButton.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.StopRecordingButton.Location = new System.Drawing.Point(215, 75);
+            this.StopRecordingButton.Name = "StopRecordingButton";
+            this.StopRecordingButton.Size = new System.Drawing.Size(75, 36);
+            this.StopRecordingButton.TabIndex = 3;
+            this.StopRecordingButton.Text = "Остановить запись";
+            this.StopRecordingButton.UseVisualStyleBackColor = false;
+            this.StopRecordingButton.Click += new System.EventHandler(this.StopRecordingButton_Click);
+            // 
+            // RepeatCheckBox
+            // 
+            this.RepeatCheckBox.AutoSize = true;
+            this.RepeatCheckBox.Location = new System.Drawing.Point(139, 116);
+            this.RepeatCheckBox.Name = "RepeatCheckBox";
+            this.RepeatCheckBox.Size = new System.Drawing.Size(116, 17);
+            this.RepeatCheckBox.TabIndex = 4;
+            this.RepeatCheckBox.Text = "Повтор раз в(мс):";
+            this.RepeatCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // RepeatTimeTextBox
+            // 
+            this.RepeatTimeTextBox.Location = new System.Drawing.Point(246, 114);
+            this.RepeatTimeTextBox.Name = "RepeatTimeTextBox";
+            this.RepeatTimeTextBox.Size = new System.Drawing.Size(43, 20);
+            this.RepeatTimeTextBox.TabIndex = 5;
+            // 
+            // RunMacrosesButton
+            // 
+            this.RunMacrosesButton.BackColor = System.Drawing.Color.FloralWhite;
+            this.RunMacrosesButton.Location = new System.Drawing.Point(139, 196);
+            this.RunMacrosesButton.Name = "RunMacrosesButton";
+            this.RunMacrosesButton.Size = new System.Drawing.Size(151, 23);
+            this.RunMacrosesButton.TabIndex = 6;
+            this.RunMacrosesButton.Text = "Запустить макросы";
+            this.RunMacrosesButton.UseVisualStyleBackColor = false;
+            this.RunMacrosesButton.Click += new System.EventHandler(this.RunMacrosesButton_Click);
+            // 
+            // StopMacrosesButton
+            // 
+            this.StopMacrosesButton.BackColor = System.Drawing.Color.FloralWhite;
+            this.StopMacrosesButton.Location = new System.Drawing.Point(139, 219);
+            this.StopMacrosesButton.Name = "StopMacrosesButton";
+            this.StopMacrosesButton.Size = new System.Drawing.Size(151, 23);
+            this.StopMacrosesButton.TabIndex = 7;
+            this.StopMacrosesButton.Text = "Остановить макросы";
+            this.StopMacrosesButton.UseVisualStyleBackColor = false;
+            this.StopMacrosesButton.Click += new System.EventHandler(this.StopMacrosesButton_Click);
+            // 
+            // MacrosNameLabel
+            // 
+            this.MacrosNameLabel.AutoSize = true;
+            this.MacrosNameLabel.Location = new System.Drawing.Point(183, 39);
+            this.MacrosNameLabel.Name = "MacrosNameLabel";
+            this.MacrosNameLabel.Size = new System.Drawing.Size(60, 13);
+            this.MacrosNameLabel.TabIndex = 8;
+            this.MacrosNameLabel.Text = "Название:";
+            // 
+            // MacrosNameTextBox
+            // 
+            this.MacrosNameTextBox.BackColor = System.Drawing.Color.White;
+            this.MacrosNameTextBox.Location = new System.Drawing.Point(140, 53);
+            this.MacrosNameTextBox.Name = "MacrosNameTextBox";
+            this.MacrosNameTextBox.Size = new System.Drawing.Size(150, 20);
+            this.MacrosNameTextBox.TabIndex = 9;
+            // 
+            // SetActivationKeyButton
+            // 
+            this.SetActivationKeyButton.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.SetActivationKeyButton.Location = new System.Drawing.Point(140, 131);
+            this.SetActivationKeyButton.Name = "SetActivationKeyButton";
+            this.SetActivationKeyButton.Size = new System.Drawing.Size(150, 35);
+            this.SetActivationKeyButton.TabIndex = 10;
+            this.SetActivationKeyButton.Text = "Задать клавишу активации";
+            this.SetActivationKeyButton.UseVisualStyleBackColor = false;
+            this.SetActivationKeyButton.Click += new System.EventHandler(this.SetActivationKeyButton_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Макросы";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // DeleteSelected
+            // 
+            this.DeleteSelected.BackColor = System.Drawing.Color.Coral;
+            this.DeleteSelected.Location = new System.Drawing.Point(139, 10);
+            this.DeleteSelected.Name = "DeleteSelected";
+            this.DeleteSelected.Size = new System.Drawing.Size(150, 26);
+            this.DeleteSelected.TabIndex = 11;
+            this.DeleteSelected.Text = "Удалить выделенный";
+            this.DeleteSelected.UseVisualStyleBackColor = false;
+            this.DeleteSelected.Click += new System.EventHandler(this.DeleteSelected_Click);
+            // 
+            // InfoButton
+            // 
+            this.InfoButton.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.InfoButton.Location = new System.Drawing.Point(110, 13);
+            this.InfoButton.Name = "InfoButton";
+            this.InfoButton.Size = new System.Drawing.Size(22, 23);
+            this.InfoButton.TabIndex = 12;
+            this.InfoButton.Text = "I";
+            this.InfoButton.UseVisualStyleBackColor = false;
+            this.InfoButton.Click += new System.EventHandler(this.InfoButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(294, 252);
+            this.Controls.Add(this.InfoButton);
+            this.Controls.Add(this.DeleteSelected);
+            this.Controls.Add(this.SetActivationKeyButton);
+            this.Controls.Add(this.MacrosNameTextBox);
+            this.Controls.Add(this.MacrosNameLabel);
+            this.Controls.Add(this.StopMacrosesButton);
+            this.Controls.Add(this.RunMacrosesButton);
+            this.Controls.Add(this.RepeatTimeTextBox);
+            this.Controls.Add(this.RepeatCheckBox);
+            this.Controls.Add(this.StopRecordingButton);
+            this.Controls.Add(this.StartRecordingButton);
+            this.Controls.Add(this.CreateMacrosButton);
+            this.Controls.Add(this.ListOfMacroses);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximumSize = new System.Drawing.Size(310, 290);
+            this.MinimumSize = new System.Drawing.Size(310, 290);
+            this.Name = "MainForm";
+            this.Text = "Макросы";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox ListOfMacroses;
+        private System.Windows.Forms.Button CreateMacrosButton;
+        private System.Windows.Forms.Button StartRecordingButton;
+        private System.Windows.Forms.Button StopRecordingButton;
+        private System.Windows.Forms.CheckBox RepeatCheckBox;
+        private System.Windows.Forms.TextBox RepeatTimeTextBox;
+        private System.Windows.Forms.Button RunMacrosesButton;
+        private System.Windows.Forms.Button StopMacrosesButton;
+        private System.Windows.Forms.Label MacrosNameLabel;
+        private System.Windows.Forms.TextBox MacrosNameTextBox;
+        private System.Windows.Forms.Button SetActivationKeyButton;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button DeleteSelected;
+        private System.Windows.Forms.Button InfoButton;
+    }
+}
+
---
+++ MacrosCreator/Form1.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.IO;
+using System.Xml.Serialization;
+using KeyboardHook;
+
+namespace MacrosCreator
+{
+    public partial class MainForm : Form
+    {
+        const string safePath = "safe.xml";
+        XmlSerializer serializer = new XmlSerializer(typeof(List<Macros>));
+
+        List<Macros> Macroses = new List<Macros>(0);
+        LowLevelKeyboardListener Listener;
+        bool MacrosesActive;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            notifyIcon1.Visible = false;
+
+            LoadMacroses();
+            AddMacroses();
+
+            MacrosesActive = false;
+
+            LastRecordedKeys = new List<Key>(0);
+            LastActivationKey = default(Key);
+
+            StopMacrosesButton.Enabled = false;
+
+            Listener = new LowLevelKeyboardListener();
+            Listener.OnKeyPressed += Listener_OnKeyPressed;
+            Listener.HookKeyboard();
+        }
+
+        private void Listener_OnKeyPressed(object sender, KeyPressedArgs e)
+        {
+            if (MacrosesActive)
+            {
+                foreach (string s in ListOfMacroses.CheckedItems.OfType<string>())
+                {
+                    for (int x = 0; x < Macroses.Count; x++)
+                        if (Macroses[x].Name == s)
+                            Macroses[x].RunMacros(e.KeyPressed);
+                }
+            }
+        }
+
+        void LoadMacroses()
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(safePath, FileMode.Open))
+                {
+                    Macroses = (List<Macros>)serializer.Deserialize(fs);
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("Файл не найден");
+            }
+        }
+
+        void SafeMacroses()
+        {
+            using (FileStream fs = new FileStream(safePath, FileMode.OpenOrCreate))
+            {
+                serializer.Serialize(fs, Macroses);
+            }
+        }
+
+        void AddMacroses()
+        {
+            foreach (var m in Macroses)
+                ListOfMacroses.Items.Add(m.Name);
+        }
+
+        private void CreateMacrosButton_Click(object sender, EventArgs e)
+        {
+            if (MacrosNameTextBox.Text == "") { MessageBox.Show("Введите название макроса"); return; }
+            if (RepeatTimeTextBox.Text == "") { MessageBox.Show("Введите время повтора"); return; }
+            int RepeatTime;
+            try { RepeatTime = Convert.ToInt32(RepeatTimeTextBox.Text); }
+            catch { MessageBox.Show("Вводите нормальные числа"); return; }
+            if (LastRecordedKeys.Count == 0) { MessageBox.Show("Запишите макрос"); return; }
+            if (LastActivationKey == default(Key)) { MessageBox.Show("Задайте клавишу активации"); return; }
+
+            Macroses.Add(new Macros(MacrosNameTextBox.Text, LastRecordedKeys, LastActivationKey, RepeatTime));
+            Listener.OnKeyPressed -= RecordingHandler;
+            ListOfMacroses.Items.Add(MacrosNameTextBox.Text);
+
+            LastRecordedKeys = new List<Key>(0);
+            LastActivationKey = default(Key);
+
+            MessageBox.Show(Macroses[Macroses.Count - 1].ToString());
+        }
+
+        List<Key> LastRecordedKeys;
+        private void StartRecordingButton_Click(object sender, EventArgs e)
+        {
+            Listener.OnKeyPressed += RecordingHandler;
+
+            StartRecordingButton.Enabled = false;
+            DeleteSelected.Enabled = false;
+            MacrosNameTextBox.Enabled = false;
+            InfoButton.Enabled = false;
+            SetActivationKeyButton.Enabled = false;
+            CreateMacrosButton.Enabled = false;
+            RunMacrosesButton.Enabled = false;
+            StopMacrosesButton.Enabled = false;
+            RepeatTimeTextBox.Enabled = false;
+            ListOfMacroses.Enabled = false;
+        }
+
+        private void RecordingHandler(object sender, KeyPressedArgs e)
+        {
+            LastRecordedKeys.Add(e.KeyPressed);
+        }
+
+        private void StopRecordingButton_Click(object sender, EventArgs e)
+        {
+            Listener.OnKeyPressed -= RecordingHandler;
+
+            StartRecordingButton.Enabled = true;
+            DeleteSelected.Enabled = true;
+            MacrosNameTextBox.Enabled = true;
+            InfoButton.Enabled = true;
+            SetActivationKeyButton.Enabled = true;
+            CreateMacrosButton.Enabled = true;
+            RunMacrosesButton.Enabled = true;
+            StopMacrosesButton.Enabled = true;
+            RepeatTimeTextBox.Enabled = true;
+            ListOfMacroses.Enabled = true;
+        }
+
+        Key LastActivationKey;
+        private void SetActivationKeyButton_Click(object sender, EventArgs e)
+        {
+            Listener.OnKeyPressed += SetActivationKeyHandler;
+        }
+
+        private void SetActivationKeyHandler(object sender, KeyPressedArgs e)
+        {
+            LastActivationKey = e.KeyPressed;
+            Listener.OnKeyPressed -= SetActivationKeyHandler;
+        }
+
+        private void RunMacrosesButton_Click(object sender, EventArgs e)
+        {
+            MacrosesActive = true;
+            RunMacrosesButton.Enabled = false;
+            StopMacrosesButton.Enabled = true;
+        }
+
+        private void StopMacrosesButton_Click(object sender, EventArgs e)
+        {
+            MacrosesActive = false;
+            RunMacrosesButton.Enabled = true;
+            StopMacrosesButton.Enabled = false;
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.WindowState = FormWindowState.Normal;
+                this.ShowInTaskbar = true;
+                notifyIcon1.Visible = false;
+            }
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.ShowInTaskbar = false;
+                notifyIcon1.Visible = true;
+                notifyIcon1.BalloonTipText = "Приложение свернуто";
+                notifyIcon1.ShowBalloonTip(500);
+            }
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SafeMacroses();
+            Listener.UnHookKeyboard();
+        }
+
+        private void DeleteSelected_Click(object sender, EventArgs e)
+        {
+            for (int x = 0; x < ListOfMacroses.Items.Count; x++)
+            {
+                if (ListOfMacroses.GetSelected(x))
+                {
+                    ListOfMacroses.Items.RemoveAt(x);
+                    Macroses.RemoveAt(x);
+                }
+            }
+        }
+
+        private void InfoButton_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int x = 0; x < Macroses.Count; x++)
+                sb.Append(Macroses[x].ToString() + "\n================================\n");
+            MessageBox.Show(sb.ToString(), "Информация о макросах");
+        }
+    }
+
+    [Serializable]
+    public struct Macros
+    {
+        public string Name;
+        public string[] Keys;
+        public Key ActivationKey;
+        public int Repeat;
+
+        public Macros(string name, List<Key> keys, Key activationKey, int repeat)
+        {
+            Name = name;
+            Keys = GetNormKeys(keys);
+            ActivationKey = activationKey;
+            Repeat = repeat;
+        }
+
+        public void RunMacros(Key key)
+        {
+            if (ActivationKey == key)
+            {
+                SendKeys.SendWait(Sum(Keys, ""));
+                System.Threading.Thread.Sleep(Repeat);
+            }
+        }
+
+        public static string[] GetNormKeys(List<Key> keys)
+        {
+            string[] normKeys = new string[keys.Count];
+            for (int x = 0; x < normKeys.Length; x++)
+                normKeys[x] = ToNormKey(keys[x]);
+            return normKeys;
+        }
+
+        static KeyConverter converter = new KeyConverter();
+
+        public static string ToNormKey(Key key)
+        {
+            switch (key)
+            {
+                case Key.N: return "n";
+                case Key.B: return "b";
+                case Key.V: return "v";
+                case Key.C: return "c";
+                case Key.X: return "x";
+                case Key.Z: return "z";
+                case Key.L: return "l";
+                case Key.K: return "k";
+                case Key.J: return "j";
+                case Key.H: return "h";
+                case Key.G: return "g";
+                case Key.F: return "f";
+                case Key.D: return "d";
+                case Key.S: return "s";
+                case Key.A: return "a";
+                case Key.P: return "p";
+                case Key.O: return "o";
+                case Key.I: return "i";
+                case Key.U: return "u";
+                case Key.Y: return "y";
+                case Key.T: return "t";
+                case Key.R: return "r";
+                case Key.E: return "e";
+                case Key.W: return "w";
+                case Key.Q: return "q";
+                case Key.M: return "m";
+                case Key.F16: return "{F16}";
+                case Key.F15: return "{F15}";
+                case Key.F14: return "{F14}";
+                case Key.F13: return "{F13}";
+                case Key.F12: return "{F12}";
+                case Key.F11: return "{F11}";
+                case Key.F10: return "{F10}";
+                case Key.F9: return "{F9}";
+                case Key.F8: return "{F8}";
+                case Key.F7: return "{F7}";
+                case Key.F6: return "{F6}";
+                case Key.F5: return "{F5}";
+                case Key.F4: return "{F4}";
+                case Key.F3: return "{F3}";
+                case Key.F2: return "{F2}";
+                case Key.F1: return "{F1}";
+                case Key.NumPad9: return "9";
+                case Key.NumPad8: return "8";
+                case Key.NumPad7: return "7";
+                case Key.NumPad6: return "6";
+                case Key.NumPad5: return "5";
+                case Key.NumPad4: return "4";
+                case Key.NumPad3: return "3";
+                case Key.NumPad2: return "2";
+                case Key.NumPad1: return "1";
+                case Key.NumPad0: return "0";
+                case Key.Space: return "{SPACE}";
+                case Key.Enter: return "{ENTER}";
+                case Key.LeftShift: return "+";
+                case Key.LeftCtrl: return "^";
+                case Key.LeftAlt: return "%";
+                case Key.D9: return "9";
+                case Key.D8: return "8";
+                case Key.D7: return "7";
+                case Key.D6: return "6";
+                case Key.D5: return "5";
+                case Key.D4: return "4";
+                case Key.D3: return "3";
+                case Key.D2: return "2";
+                case Key.D1: return "1";
+                case Key.D0: return "0";
+                case Key.Up: return "{UP}";
+                case Key.Down: return "{DOWN}";
+                case Key.Left: return "{LEFT}";
+                case Key.Right: return "{RIGHT}";
+                case Key.Tab: return "{TAB}";
+                case Key.Escape: return "{ESC}";
+                case Key.CapsLock: return "{CAPSLOCK}";
+            }
+
+            return key.ToString();
+        }
+
+        public override string ToString()
+        {
+            return "Имя: " + Name + "\nМакрос: " + Sum(Keys, " ") + "\nКлавиша активации: " + ActivationKey.ToString() + "\nПовтор раз в: " + Repeat + " мс";
+        }
+
+        string Sum<T>(T[] list, string r)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int x = 0; x < list.Length; x++)
+                sb.Append(list[x].ToString() + r);
+            return sb.ToString();
+        }
+    }
+}
